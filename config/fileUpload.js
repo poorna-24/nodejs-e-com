d5f8cb03445fcd1a7dc7d0adb36d68c0988d5e0a
Poorna Chander Terala <60097572+poorna-24@users.noreply.github.com>
2024-06-20 14:32:57 +0530

    env to text

--- config/fileUpload.js
+++ config/fileUpload.js
@@ -6,9 +6,12 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 //configure cloudinary
 const cloudinary = cloudinaryPackage.v2;
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+  // cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  // api_key: process.env.CLOUDINARY_API_KEY,
+  // api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+  cloud_name: "dkwsddbyk",
+  api_key: 975381911792759,
+  api_secret: "1AnQHW8A85tmAURaXa8XK10YMeI",
 });
 
 // Create storage engine for Multer
